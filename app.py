c233ce36962a2cf3c83b95c0fecc90d1449ae5b5
Yashwanthmn8 <71122092+Yashwanthmn8@users.noreply.github.com>
2025-03-28 10:41:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Streamlit on AWS App Runner", layout="centered")
+
+st.title("🚀 Hello from Streamlit on App Runner!")
+st.write("This app is running on AWS App Runner via a Docker container.")
+st.success("Deployment was successful!")
